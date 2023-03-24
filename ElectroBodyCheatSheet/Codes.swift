9a4264dca9ec08a3e6d3ee7180bb6dea683d18a3
Adam Tokarski <a.tokarski126@gmail.com>
2023-03-24 22:00:12 +0100

    add base view and pictures

---
+++ ElectroBodyCheatSheet/Codes.swift
@@ -0,0 +1,15 @@
+//
+//  Codes.swift
+//  ElectroBodyCheatSheet
+//
+//  Created by Adam on 24/03/2023.
+//
+
+import Foundation
+
+struct Codes {
+    static let page: [Int] = [6, 7, 8, 9, 10, 11, 13, 14, 18, 19, 20]
+    static let paragraph: [Int] = [1, 2, 3, 4, 5, 6, 7]
+    static let word: [Int] = [1, 2, 3, 4, 5, 6, 7, 10]
+    static let letter: [Int] = [1, 2, 3, 4, 5, 6, 7, 9]
+}
